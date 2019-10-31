21e261fe64a19f6fe530ab497ede09393204fd20
realprinceviper <idrisadeniji01@gmail.com>
2019-10-31 09:21:03 -0400

    create help notes for add command

--- app.js
+++ app.js
@@ -4,7 +4,21 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
-const argv = yargs.argv;
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: {
+            describe: 'Title of note',
+            demand: true,
+            alias : 't'
+        },
+        body: {
+            describe: 'Body of note',
+            demand: true,
+            alias: 'b'
+        }
+    })
+    .help()
+    .argv;
 let command = argv._[0];
 
 //console.log('Command: ' , command);
